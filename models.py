ee37b7063ac3e27b47dad573af4b3f303b719c8c
minseo <cuti0223@gmail.com>
2025-04-09 04:52:44 +0900

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    flag = db.Column(db.Integer, default=0)
